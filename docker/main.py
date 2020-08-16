41a00866b929b968309339e8aaf59e1c1b9c72de
Dmitry Antonyuk <dantonyuk@hylamobile.com>
2020-08-15 20:36:25 -0500

    Print event contents

--- docker/main.py
+++ docker/main.py
@@ -1,4 +1,9 @@
 import os
+from pathlib import Path
 
 if __name__ == "__main__":
     print(os.environ)
+    event_path = os.environ['GITHUB_EVENT_PATH']
+    contents = Path(event_path).read_text()
+    print(contents)
+
