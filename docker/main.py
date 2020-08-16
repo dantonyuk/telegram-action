6283373d598673d0d7465bd401b0227f90dceaa9
Dmitry Antonyuk <dantonyuk@hylamobile.com>
2020-08-16 12:18:24 -0500

    Send notification to Telegram

--- docker/main.py
+++ docker/main.py
@@ -1,9 +1,33 @@
-import os
+import os, sys, inspect
+import requests
 from pathlib import Path
 
-if __name__ == "__main__":
-    print(os.environ)
+def event_payload():
     event_path = os.environ['GITHUB_EVENT_PATH']
-    contents = Path(event_path).read_text()
+    return Path(event_path).read_text()
+
+
+def notify(message):
+    chat_id = os.environ['destination']
+    token = os.environ['token']
+
+    requests.post('', json={
+        'chat_id': ,
+        'parse_mode': 'MarkdownV2',
+        'disable_web_page_preview': True,
+        'text': message
+    })
+
+if __name__ == "__main__":
+    env = os.environ
+    print(env)
+    event_name = os.environ['GITHUB_EVENT_NAME']
+
+    funcs = [obj for name, obj in inspect.getmembers(sys.modules[__name__]) if name == event_name]
+    if funcs:
+        funcs[0]()
+    else:
+        notify(f"{event_name} in [{env['GITHUB_REPOSITORY']}]({env['GITHUB_SERVER_URL']}/{env['GITHUB_REPOSITORY']})")
+
     print(contents)
 
